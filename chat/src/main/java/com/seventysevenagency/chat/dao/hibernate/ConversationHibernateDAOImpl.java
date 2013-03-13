30ffcca5528b519c87f4906d1a4acf92d0211413
Daniil Kalnin <d.kalnin@77agency.com>
2013-03-13 11:36:42 +0200

    Implemented ConversationDao for Hibernate

--- chat/src/main/java/com/seventysevenagency/chat/dao/hibernate/ConversationHibernateDAOImpl.java
+++ chat/src/main/java/com/seventysevenagency/chat/dao/hibernate/ConversationHibernateDAOImpl.java
@@ -3,6 +3,7 @@ package com.seventysevenagency.chat.dao.hibernate;
 import java.util.List;
 
 import org.hibernate.Query;
+import org.hibernate.criterion.Order;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,48 +11,48 @@ import com.seventysevenagency.chat.dao.ConversationDAO;
 import com.seventysevenagency.chat.dao.DAOException;
 import com.seventysevenagency.chat.domain.Conversation;
 import com.seventysevenagency.chat.domain.Message;
+
 @Repository
 @Transactional
-public class ConversationHibernateDAOImpl extends BaseHibernateDAO implements ConversationDAO {
+public class ConversationHibernateDAOImpl extends BaseHibernateDAO implements
+		ConversationDAO {
 
 	public Long create(Conversation conversation) throws DAOException {
-		// TODO Auto-generated method stub
-		return null;
+		return (Long) getSession().save(conversation);
 	}
 
 	public int deleteById(Long id) throws DAOException {
-		// TODO Auto-generated method stub
-		return 0;
+		Query query = getSession().createQuery(
+				"DELETE Conversation WHERE id = :id");
+		query.setParameter("id", id);
+		int result = query.executeUpdate();
+
+		return result;
 	}
 
 	public int delete(Conversation conversation) throws DAOException {
-		// TODO Auto-generated method stub
-		return 0;
+		getSession().delete(conversation);
+		return 1;
 	}
 
 	public Conversation select(Long id) throws DAOException {
-		// TODO Auto-generated method stub
-		return null;
+		return (Conversation) getSession()
+				.createQuery("FROM Conversation WHERE id = :id")
+				.setParameter("id", id).uniqueResult();
 	}
 
 	public Conversation selectLast() throws DAOException {
-		// TODO Auto-generated method stub
-		return null;
+		return (Conversation) getSession().createCriteria(Conversation.class)
+				.addOrder(Order.desc("id")).setMaxResults(1).uniqueResult();
 	}
 
 	public void update(Conversation conversation) throws DAOException {
-		// TODO Auto-generated method stub
-		
+		getSession().update(conversation);
+
 	}
 
 	public List<Conversation> selectAll() {
-		List<Conversation> list = null;
-		Query query = getSession()
-				.createQuery("FROM Conversation");
-		list = query.list();
-		return list;
+		return getSession().createQuery("FROM Conversation").list();
 	}
-
-	
 
 }
